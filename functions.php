2aba92738a14c0a061e1249bcac505e0666538a4
ylafaro0310 <y16mukurou@gmail.com>
2021-01-30 20:12:09 +0900

    カテゴリページを修正

--- functions.php
+++ functions.php
@@ -65,7 +65,7 @@ function himajin_pagination($pages='',$range=3) {
 
 // Limit posts per page
 function himajin_limit_posts( $query ) {
-  if ( $query->is_home() && $query->is_main_query() ) {
+  if ( ($query->is_home() || $query->is_archive()) && $query->is_main_query() ) {
       $query->set( 'post_type', 'post' );
       $query->set( 'posts_per_page', '9' );
   }
